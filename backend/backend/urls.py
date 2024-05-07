bb4bf0dbd9abc5150a43fc29cf7d0a203f92a6a7
natanMedina <natan.medina@correounivalle.edu.co>
2024-05-06 20:56:09 -0500

    2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,7 +12,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 #ola laura 
-#ola satanas ----
+#ola satanas ----fvgb h fdaf 
 
 router = routers.DefaultRouter()
 router.register('api/users', UsuarioViewSet, 'nombra_urls_usuarios')
